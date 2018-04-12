5a834a23d5fe8f2af70ce95416310be3aecad2ba
John Navitsky <johnn@cray.com>
2018-04-12 06:22:03 -0500

    Added ability to print records from a specific donor.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor
 
 app = Flask(__name__)
 
@@ -15,7 +15,15 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+@app.route('/donations/<donor>/')
+def get_donor(donor):
+    """
+    print all records for a specfic donor
+    """
+    # downselect data to only records matching specified donor
+    donations = Donation.select().join(Donor).where(Donor.name == donor)
+    return render_template('donations.jinja2', donations=donations)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
